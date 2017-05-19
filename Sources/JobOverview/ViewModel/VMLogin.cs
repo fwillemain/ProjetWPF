121f56a767cfdf150411d5bd9e4b9053a4d8a1c3
glavender <glavender@GLAVENDER17-DE>
2017-05-19 13:45:47 +0200

    Enregistrement personne courante

--- Sources/JobOverview/ViewModel/VMLogin.cs
+++ Sources/JobOverview/ViewModel/VMLogin.cs
@@ -1,21 +1,35 @@
-﻿using System;
+﻿using JobOverview.Entity;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace JobOverview.ViewModel
 {
 	public class VMLogin : ViewModelBase
 	{
 		// TODO : à remplacer par une vraie liste de personnes
-		public List<string> Personnes { get; private set; }
+		public List<Employe> ListPeople { get; private set; }
+        public Employe LastEmploye { get; set; }
 
-		public VMLogin()
+        public VMLogin()
 		{
-			// TODO : à remplacer par un appel à une méthode de DAL
-			Personnes = new List<string>()	{ "Pierre", "Paul", "Jacques" };
-		}
-	}
+            // TODO : à remplacer par un appel à une méthode de DAL
+            ListPeople = new List<Employe>();
+            ListPeople.Add(new Employe { Id = 1, LastName = "Jean-Paul" });
+            ListPeople.Add(new Employe { Id = 2, LastName = "Bidule" });
+            if (ListPeople.Select(c => c.Id).Contains(Properties.Settings.Default.EmployeId))
+                LastEmploye = ListPeople.Where(c => c.Id == Properties.Settings.Default.EmployeId).FirstOrDefault();
+            else LastEmploye = ListPeople[0];
+        }
+        public override ValidationResult Validate()
+        {
+            Properties.Settings.Default.EmployeId = LastEmploye.Id;
+            Properties.Settings.Default.Save();
+            return base.Validate();
+        }
+    }
 }
