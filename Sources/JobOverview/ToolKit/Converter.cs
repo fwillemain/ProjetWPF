d871e26e8ddf20774d0829957450b80f86244ef4
glavender <glavender@GLAVENDER17-DE>
2017-05-22 14:52:45 +0200

    filtre consultation opérationnel

--- Sources/JobOverview/ToolKit/Converter.cs
+++ Sources/JobOverview/ToolKit/Converter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,20 @@ namespace JobOverview.ToolKit
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return (float)value == 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DebuggerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Debugger.Break();
+            return value;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
