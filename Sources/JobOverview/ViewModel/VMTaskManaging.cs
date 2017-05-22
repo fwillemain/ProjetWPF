4cb79fcd847e2138e4733b016006e25db8205f57
lmilbor <lmilbor@LMILBOR17-DE>
2017-05-22 10:12:34 +0200

    TaskManaging à tester

--- Sources/JobOverview/ViewModel/VMTaskManaging.cs
+++ Sources/JobOverview/ViewModel/VMTaskManaging.cs
@@ -2,7 +2,9 @@
 using JobOverview.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,8 +15,16 @@ namespace JobOverview.ViewModel
         public List<Employee> ListEmployee { get; set; }
         public VMTaskManaging()
         {
-            //ListEmployee = DAL.GetListEmployeeWithoutTask(Properties.Settings.Default.EmployeeId);
-            //ListEmployee.Add();
+            ListEmployee = DAL.GetListEmployeeWithoutTask(Properties.Settings.Default.EmployeeId);
+            ListEmployee.Add(VMMain.CurrentEmployee);
         }
+
+        #region Gestion de MAJ de l'affichage après MAJ des données
+        private void RaisePropertyChanged([CallerMemberName] string prop = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        #endregion
     }
 }
