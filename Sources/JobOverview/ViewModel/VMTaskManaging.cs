c99561a731211389ec374cbc1e28894bd14c8c8a
lmilbor <lmilbor@LMILBOR17-DE>
2017-05-22 11:09:32 +0200

    -Correction du lien UML entre Module et Software -Gestion du binding entre la liste d'employee et la liste de tache.

--- Sources/JobOverview/ViewModel/VMTaskManaging.cs
+++ Sources/JobOverview/ViewModel/VMTaskManaging.cs
@@ -8,19 +8,39 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace JobOverview.ViewModel
 {
     public class VMTaskManaging : ViewModelBase
     {
+        #region Champs privé et propriétés
+        private Employee _selectedEmployee;
+        public Employee SelectedEmployee
+        {
+            get
+            { return _selectedEmployee; }
+            set
+            {
+                if (_listEmployee.Where(e => e.Login == value.Login).FirstOrDefault().ListTask == null)
+                {
+                    _listEmployee.Where(e => e.Login == value.Login).FirstOrDefault().ListTask = DAL.GetListTask(_selectedEmployee.Login);
+                }
+                SetProperty(ref _selectedEmployee, value);
+            }
+        }
         private ObservableCollection<Employee> _listEmployee;
         public ObservableCollection<Employee> ListEmployee
-        { get
+        {
+            get
             { return _listEmployee; }
-            set { SetProperty(ref _listEmployee, value); } }
+            set { SetProperty(ref _listEmployee, value); }
+        }
+        #endregion
+
         public VMTaskManaging(List<Employee> listEmployee)
         {
-            ListEmployee = listEmployee.Where( e => e.CodeTeam == VMMain.CurrentEmployee.CodeTeam);
+            ListEmployee = (ObservableCollection<Employee>)listEmployee.Where(e => e.CodeTeam == VMMain.CurrentEmployee.CodeTeam);
         }
     }
 }
